3ee6263aac57e2685f3c21862c65e75173bcd0ba
Russell Sim <russell@vpac.org>
2009-07-08 13:08:38 +1000

    updates egg information

--- setup.py
+++ setup.py
@@ -5,18 +5,23 @@ version = '1.0'
 
 setup(name='arcs.gsi',
       version=version,
-      description="",
+      description="Library to assist GSI authentication and certificate handling in python.",
       long_description=open(os.path.join("README")).read() + "\n" +
                        open(os.path.join("CHANGES")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Topic :: Security :: Cryptography",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Distributed Computing",
         ],
       keywords='',
       author='Russell Sim',
       author_email='russell.sim@arcs.org.au',
-      url='',
+      url='http://code.arcs.org.au/gitorious/arcs-gsi/arcs-gsi',
+      download_url='http://code.arcs.org.au/pypi/',
       license='GPL',
       packages=find_packages('src', exclude=['ez_setup']),
       package_dir = {'': 'src'},
