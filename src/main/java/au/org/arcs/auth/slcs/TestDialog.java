ca227f6bb0a0fcdd89db945809ff04e3c20d8a41
Markus Binsteiner <markus@avus.(none)>
2009-09-21 17:41:49 +1000

    Adding slcs swing panel.

---
+++ src/main/java/au/org/arcs/auth/slcs/TestDialog.java
@@ -0,0 +1,43 @@
+package au.org.arcs.auth.slcs;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class TestDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			TestDialog dialog = new TestDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public TestDialog() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			SLCSPanel panel = new SLCSPanel();
+			contentPanel.add(panel);
+		}
+	}
+
+}
