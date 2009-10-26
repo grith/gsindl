21863e62a44b9644b1849d737c88932bcf8d2dfe
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-10-26 15:53:21 +1100

    Added example code to SLCS.

--- src/main/java/au/org/arcs/auth/slcs/SLCS.java
+++ src/main/java/au/org/arcs/auth/slcs/SLCS.java
@@ -30,6 +30,7 @@ import org.bouncycastle.asn1.x509.X509Extensions;
 import org.bouncycastle.asn1.x509.X509Name;
 import org.bouncycastle.jce.PKCS10CertificationRequest;
 import org.bouncycastle.openssl.PEMWriter;
+import org.python.core.Py;
 import org.python.core.PyDictionary;
 import org.python.core.PyInstance;
 import org.python.core.PyList;
@@ -43,6 +44,7 @@ import au.org.arcs.auth.shibboleth.IdpObject;
 import au.org.arcs.auth.shibboleth.ShibListener;
 import au.org.arcs.auth.shibboleth.ShibLoginEventSource;
 import au.org.arcs.auth.shibboleth.Shibboleth;
+import au.org.arcs.auth.shibboleth.ShibbolethClient;
 
 public class SLCS implements ShibListener {
 
@@ -341,7 +343,88 @@ public class SLCS implements ShibListener {
 	}
 
 	public void shibLoginStarted() {
-		// TODO Auto-generated method stub
+
+		// do nothing
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		final String idp = "VPAC";
+		final String username = "markus";
+		// I know, the password should be a char[]. But that doesn't work with the jython bindings and it would be useless in 
+		// this case anyway since python uses plain strings in memory.
+		final String password = args[0];
+		
+		// instead of creating those objects inline you could also use the StaticIdpObject and StaticCredentialManager classes, 
+		// which would be probably easier...
+		IdpObject idpObject = new IdpObject() {
+			
+			@Override
+			public PyInstance prompt(ShibbolethClient shibboleth) {
+				
+				// since we already know the idp, we tell the shibboleth object to run straight way.
+				// if we had an IdpObject which is for example an interactive gui element we would do it differently.
+				// this is a bit weird, I know, but Shib is to blame for that. Ask Russell if you need to know more...
+				shibboleth.run();
+				return null;
+			}
+			
+			@Override
+			public String get_idp() {
+				// return the idp
+				return idp;
+			}
+		};
+		
+		CredentialManager credentialManager = new CredentialManager() {
+			
+			public void set_title(String title) {
+				// only needed if you want to display an interactive login page
+			}
+			
+			public PyObject prompt(Object shibboleth) {
+				
+				// this is another one of the confusing things that need to be done. Russell is your man.
+				return Py.java2py(shibboleth).invoke("run");
+
+			}
+			
+			public String get_username() {
+				// the username
+				return username;
+			}
+			
+			public String get_password() {
+				// the password
+				return password;
+			}
+		};
+		
+		SlcsListener slcsListener = new SlcsListener() {
+			
+			public void slcsLoginFailed(String message, Exception optionalException) {
+
+				System.out.println("Slcs login failed: "+message);
+			}
+			
+			public void slcsLoginComplete(X509Certificate cert, PrivateKey privateKey) {
+
+				
+				System.out.println("Slcs login complete.");
+				
+				// now do something with your certificate
+			}
+		};
+		
+		
+		Shibboleth shibboleth = new Shibboleth(idpObject, credentialManager);
+		SLCS slcs = new SLCS(shibboleth);
+		slcs.addSlcsListener(slcsListener);
+		
+		shibboleth.openurl("https://slcs1.arcs.org.au/SLCS/login");
+		
+
 		
 	}
 
